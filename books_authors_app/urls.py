604cd3378cc50e5facfc6c99c6ba1768e1cc5750
BenjaminLivingstone <benjaminlivingstone@gmail.com>
2021-08-12 19:31:23 -0400

    Books & Authors home's OK

--- books_authors_app/urls.py
+++ books_authors_app/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('addbook/', views.addbook),
+    path('authors/', views.author),
+    path('addauthor/', views.addauthor),
+    path('books/<int:num>', views.addbook),
+    path('authors/<int:num>', views.addauthor),
+
 ]
--- books_authors_app/views.py
+++ books_authors_app/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render, HttpResponse, redirect
+from . models import *
 
 def index(request):
     context = {
-        "title": "Books & Authors",
+        "title": "Add a Book",
+        "books": Book.objects.all(),
     }
     return render(request, "index.html", context)
+
+def addbook(request):
+    # print(request.POST) Verificar !!!
+    book=Book.objects.create(title=request.POST['book_title'], desc=request.POST['book_desc'])
+    return redirect("/")
+
+def author(request):
+    context = {
+        "title": "Add an Author",
+        "authors": Author.objects.all(),
+    }
+    return render(request, "authors.html", context)
+
+def addauthor(request):
+    author= Author.objects.create(first_name=request.POST['author_first_name'], last_name=request.POST['author_last_name'], notes=request.POST['author_notes'])
+    return redirect("/authors/")
 
 # def some_method(request):
 
 # 	return redirect("/") 
+
+# def books(request):
+#     context = {
+#         "title": "Add a Book",
+#     }
+#     return render(request, "books.html", context)
