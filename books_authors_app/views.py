7e48e101cedd58fa3c8a19ccdac3cd955a356589
BenjaminLivingstone <benjaminlivingstone@gmail.com>
2021-08-14 11:59:08 -0400

    Casi listo, templates ok

--- books_authors_app/views.py
+++ books_authors_app/views.py
@@ -9,7 +9,7 @@ def index(request):
     return render(request, "index.html", context)
 
 def addbook(request):
-    # print(request.POST) Verificar !!!
+    # print(request.POST) Se usa para veriicar Verificar !!!
     book=Book.objects.create(title=request.POST['book_title'], desc=request.POST['book_desc'])
     return redirect("/")
 
@@ -24,23 +24,32 @@ def addauthor(request):
     author= Author.objects.create(first_name=request.POST['author_first_name'], last_name=request.POST['author_last_name'], notes=request.POST['author_notes'])
     return redirect("/authors/")
 
-
 def bookdetail (request, num):
     context = {
         "book": Book.objects.get(id=num),
         "allauthors": Author.objects.all(),
-
         # "bookdetail": Book.objects.get(id=num).authors.all()
-        # "title":
     }
-    return render(request, "authorsandbooks.html", context)
+    return render(request, "bookdetails.html", context)
 
 def authordetail (request, num):
     context = {
-        # "title":
         "author": Author.objects.get(id=num),
         "allbooks": Book.objects.all(),
-
         # "authordetail": Book.objects.get(id=num).authors.all()
     }
-    return render(request, "authorsandbooks.html", context)
+    return render(request, "authordetails.html", context)
+
+def addbooktoauthor (request):
+    context = {
+        "author": Author.objects.get(id=num),
+        "allbooks": Book.objects.all(),
+        # "authordetail": Book.objects.get(id=num).authors.all()
+    }
+
+def addauthortobook (request):
+    context = {
+        "author": Author.objects.get(id=num),
+        "allbooks": Book.objects.all(),
+        # "authordetail": Book.objects.get(id=num).authors.all()
+    }
